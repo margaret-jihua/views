15a62ba2c2e8fdaaa68f30b34eb3021c9522decc
margare-jihua <margaret.jihua@gmail.com>
2020-08-04 20:46:24 -0700

    Routes set up

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send('This is the Home Page!')
+})
+
+app.get('/about', (req,res) => {
+    res.send('Some stuff about me will go here.')
+})
+
+app.get('/blog', (req,res) => {
+    res.send('A directory of all my blog posts will go here.')
+})
+
+app.listen(8000, () => {
+    console.log("Listening to port 8000");
+})
